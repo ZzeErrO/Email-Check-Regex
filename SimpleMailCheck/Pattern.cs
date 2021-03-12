708a85ad921f4c3ec26375c62946caaf0bd66dd7
prashikjaware <prashikjaware123@gmail.com>
2021-03-12 12:22:11 +0530

    [Prashik]Added: Check correct Email Format using REGEX

---
+++ SimpleMailCheck/Pattern.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace SimpleMailCheck
+{
+    class Pattern
+    {
+        //string Regex_Pincode = "^[a-zA-Z0-9]{1,}@[A-Za-z]+[.][A-Za-z]{2,4}$";
+        string Regex_Pincode = "^[a-z0-9A-Z]+([.#_+-][a-z0-9A-Z]+)*[@][a-z0-9A-Z]+[.][a-zA-Z]{2,3}(.[a-zA-Z]{2,3})?$";
+
+        public bool validatePinCode(string pincode) 
+        {
+            return Regex.IsMatch(pincode, Regex_Pincode);
+        }
+    }
+}
---
+++ SimpleMailCheck/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SimpleMailCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern patterns = new Pattern();
+            Console.WriteLine(patterns.validatePinCode("abc.58@gmail.com.in"));
+            Console.ReadKey();
+        }
+    }
+}
